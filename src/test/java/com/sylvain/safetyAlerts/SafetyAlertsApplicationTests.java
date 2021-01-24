53e2982ecc92973cfec5e935ed941576a17bd47a
sylvainsbr <sylvainsouber@gmail.com>
2021-01-24 20:07:24 +0100

    init

---
+++ src/test/java/com/sylvain/safetyAlerts/SafetyAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylvain.safetyAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
